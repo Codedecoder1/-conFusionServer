87479792595ba7d4ba57f13f703a7caed0c1bdb0
Eudis Matos <eudismatos@yahoo.es>
2019-06-20 14:36:46 -0700

    User Authentication with Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -47,6 +49,22 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+function auth (req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');                          
+    err.status = 403;
+    next(err);
+  }
+  else {
+        next();
+  }
+}
 
 app.use('/', index);
 app.use('/users', users);
